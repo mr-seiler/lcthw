435c00d8af52256a6d6d3ff952ad576d2f42d18c
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-21 13:02:16 -0500

    exercise 5

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* comment! b/c we can */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // single line comment
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
