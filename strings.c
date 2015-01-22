052119a5d76254535a600cd7d9c55f894cc40f3f
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-22 09:42:44 -0500

    some more string notes

--- strings.c
+++ strings.c
@@ -1,8 +1,10 @@
+/* This helped: http://stackoverflow.com/a/1095006 */
+
 #include <stdio.h>
 
 int main()
 {
-    // this is a string:
+    // this is a string, it's an array of characters terminated with null
     char str1[] = {
         's', 't', 'r', 'i', 'n', 'g', '\0'
     };
@@ -13,6 +15,8 @@ int main()
 
     // this is ALSO a string; it looks like a pointer to a char.
     // ...but it isn't.
+    // It is a pointer to a constant string stored in static memory.
+    // You can change the pointer, but not the actual string.
     char *str3 = "string";
 
     // demonstrate:
@@ -20,6 +24,8 @@ int main()
 
     // THIS, however, is an ARRAY OF STRINGS
     // it looks like a pointer to an array of chars, BUT IT ISN'T
+    // (It's an array of pointers to char, like an array of the example above this one.)
+    // An array of pointers to string constants in static memory.
     char *strarr[] = {
         "one", "two", "three"
     };
