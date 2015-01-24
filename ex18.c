ad94d302bf4d420e5e4ad896e1b69d655f9fcbf7
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-24 12:00:38 -0500

    exercise 18 - part 2
    Added another sorting algorithm, use single command line arg to specify
    how many random numbers to generate and sort, prettier output,
    callbacks galore, strange_order comparator produces different results
    for different sorting algorithms and I don't know why, etc.

--- ex18.c
+++ ex18.c
@@ -19,9 +19,11 @@ void die(const char *message)
 // function pointer type for a function that takes to ints and returns one
 typedef int (*compare_cb)(int a, int b);
 
+// function pointer type for integer sorting function that will sort
+// the numbers in-place.
+typedef void (*sort_cb)(int *numbers, int count, compare_cb cmp);
 
-// does this need to take a pointer to some ints, or a pointer to a pointer?
-// ...apparently this is fine (it works ok);
+/* Swaps two numbers in an integer array. */
 void swap(int *numbers, int idx1, int idx2)
 {
     int temp = numbers[idx1];
@@ -29,52 +31,72 @@ void swap(int *numbers, int idx1, int idx2)
     numbers[idx2] = temp;
 }
 
-/* Given a a pointer to a number (which we can use like an array)
- * and a number of ints (length of the array) and a comparison
- * fucntion pointer, sort the numbers really inefficiently.
- * Return type of int* can either point to sorted array of ints
- * or just one int (like if there was an error)
+/* Given an array of numbers, sorting function, and a comparison function,
+ * returns a pointer to a sorted copy of the array on the heap.
  */
-int *bubble_sort(int *numbers, int count, compare_cb cmp)
+int *do_sort(int *numbers, int count, sort_cb sorter, compare_cb cmp)
 {
-    int i = 0, j = 0;
-
-    // allocate enough memory for the sorted version of the array
     int *target = malloc(count * sizeof(int));
     if (!target) die("Memory error");
 
-    // copy the original array to the new location, which we will sort in-place
     memcpy(target, numbers, count * sizeof(int));
 
-    // do bubble sort of new array copy
+    sorter(target, count, cmp);
+    return target;
+}
+
+/* Implements sort_cb:
+ * Given a pointer to some numbers, the number of numbers,
+ * and a comparison function, sorts the numbers in-place using insertion sort.
+ */
+void insertion_sort(int *numbers, int count, compare_cb cmp)
+{
+    int i, j;
+
+    for (i = 1; i < count; i += 1)
+    {
+        j = i;
+        while (j > 0 && (cmp(numbers[j - 1], numbers[j]) > 0))
+        {
+            swap(numbers, j, j - 1);
+            j -= 1;
+        }
+    }
+}
+
+/* Implements sort_cb:
+ * Given a pointer to some numbers, the number of numbers, and a
+ * comparison function, sorts the numbers in-place using bubble sort.
+ */
+void bubble_sort(int *numbers, int count, compare_cb cmp)
+{
+    int i, j;
+
     for (i = 0; i < count; i += 1)
     {
         for (j = 0; j < count - 1; j += 1)
         {
-            if (cmp(target[j], target[j+1]) > 0)
+            if (cmp(numbers[j], numbers[j+1]) > 0)
             {
-                // do swap
-                swap(target, j, j + 1);
+                swap(numbers, j, j + 1);
             }
         }
     }
-
-    return target;
 }
 
-/* define a compare function for forward sorted order */
+/* Implements compare_cb (forward/ascending order) */
 int sorted_order(int a, int b)
 {
     return a - b;
 }
 
-/* define a compare function for reverse sorted order */
+/* Implements compare_cb for reverse sorted order */
 int reverse_order(int a, int b)
 {
     return b - a;
 }
 
-/* yet another compare function.  Is "strange order" actually a thing? */
+/* Implements comapre_cb... for some other order. */
 int strange_order(int a, int b)
 {
     if (a == 0 || b == 0)
@@ -87,48 +109,80 @@ int strange_order(int a, int b)
     }
 }
 
-void test_sorting(int *numbers, int count, compare_cb cmp)
+void print_ints(int *numbers, int count)
 {
     int i;
-    int *sorted = bubble_sort(numbers, count, cmp);
-
-    if (!sorted) die("Sort failed");
-
+    printf("[ ");
     for (i = 0; i < count; i += 1)
     {
-        printf("%d ", sorted[i]);
+        printf((i < count - 1 ? "%d, " : "%d") , numbers[i]);
     }
-    printf("\n");
+    printf(" ]\n");
+}
+
+void test_sorting(int *numbers, int count, sort_cb sorter, compare_cb cmp)
+{
+    int *sorted = do_sort(numbers, count, sorter, cmp);
+    if (!sorted) die("Sort failed");
+
+    // output the sorted array
+    print_ints(sorted, count);
 
     // deallocate the sorted copy
     free(sorted);
 }
 
+/* Simple extension of rand() function that only returns numbers
+ * in the range [0, ceil).  This method will break down as ceil
+ * approaches RAND_MAX. More robust solutions can be found here:
+ * http://c-faq.com/lib/randrange/html
+ * http://stackoverflow.com/a/6852396
+ */
+int rand_ceil(int ceil)
+{
+    return rand() / (RAND_MAX / ceil + 1);
+}
+
+/* Returns a pointer to an array of size `count` on the heap, where
+ * each int is initialized to a random number in the range 0..99
+ */
+int *get_random(int count)
+{
+    int i;
+    int *numbers = malloc(count * sizeof(int));
+
+    for (i = 0; i < count; i += 1)
+    {
+        numbers[i] = rand_ceil(100);
+    }
+
+    return numbers;
+}
+
 int main(int argc, char *argv[])
 {
     // must be given at least one number argument
-    if (argc < 2) die("USAGE: ./ex18 4 3 1 4 6");
+    if (argc < 2) die("USAGE: ./ex18 count");
 
-    // count of numbers
-    int count = argc - 1;
-    int i;
-    // pointer to the first real argument in arg array
-    char **inputs = argv + 1;
+    // try to get command line argument as #numbers
+    int count = atoi(argv[1]);
 
-    // allocate enough heap for the list of input numbers
-    int *numbers = malloc(count * sizeof(int));
-    if (!numbers) die("Memory error");
+    // get some random numbers, and print them out
+    int *numbers = get_random(count);
+    printf("Initial:\n");
+    print_ints(numbers, count);
 
-    // create int array from string arguments
-    for (i = 0; i < count; i += 1)
-    {
-        numbers[i] = atoi(inputs[i]);
-    }
+    // test bubble sort w/ different comparison functions
+    puts("Bubble sort:");
+    test_sorting(numbers, count, bubble_sort, sorted_order);
+    test_sorting(numbers, count, bubble_sort, reverse_order);
+    test_sorting(numbers, count, bubble_sort, strange_order);
 
-    // test w/ different comparison functions
-    test_sorting(numbers, count, sorted_order);
-    test_sorting(numbers, count, reverse_order);
-    test_sorting(numbers, count, strange_order);
+    // test insertion sort w/ different comparison functions
+    puts("Insertion sort:");
+    test_sorting(numbers, count, insertion_sort, sorted_order);
+    test_sorting(numbers, count, insertion_sort, reverse_order);
+    test_sorting(numbers, count, insertion_sort, strange_order);
 
     // free heap
     free(numbers);
