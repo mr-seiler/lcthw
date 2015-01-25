e2b848b31cf124c637bc9a538c7083fa3062a553
Matthew Seiler <theflyinghyrax@gmail.com>
2015-01-25 17:32:14 -0500

    more ex19

---
+++ ex19/ex19.c
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "ex19.h"
+
+/* Implementation of Monster type */
+
+/* Implementation of override attack method for monster.
+ * Returns 1 if the attack kills the monster.
+ */
+int Monster_attack(void *self, int damage)
+{
+    // cast the pointer passed to this method to a monster
+    Monster *monster = self;
+
+    printf("You attack %s!\n", monster->proto.description);
+    // alt, w/ macro:
+    // printf("You attack %s!\n", monster->_(description));
+
+    monster->hit_points -= damage;
+
+    if (monster->hit_points > 0)
+    {
+        puts("It is still alive");
+        return 0;
+    }
+    else
+    {
+        puts("It is dead!");
+        return 1;
+    }
+}
+
+int Monster_init(void *self)
+{
+    // cast
+    Monster *monster = self;
+    // set initial value
+    monster->hit_points = 10;
+    // success
+    return 1;
+}
+
+/* Literal monster prototype struct that sets Monster's init and attack methods.
+ * Our Object.h macros allow us to create a new (something) from SomethingProto,
+ * if this struct were not named this is would not work.
+ */
+Object MonsterProto = {
+    .init = Monster_init,
+    .attack = Monster_attack
+};
+
+
+/* Implementation of Room type */
+
+void *Room_move(void *self, Direction direction)
+{
+    Room *room = self;
+    Room *next = NULL;
+
+    if (direction == NORTH && room->north)
+    {
+        prtinf("You go north, into:\n");
+        next = room->north;
+    }
+    else if (direction == SOUTH && room->south)
+    {
+        prtinf("You go south, into:\n");
+        next = room->south;
+    }
+    else if (firection == EAST && room->east)
+    {
+        prtinf("You go east, into:\n");
+        next = room->east;
+    }
+    else if (direction == WEST && room->west)
+    {
+        prtinf("You go west, into:\n");
+        next = room->west;
+    }
+    else
+    {
+        printf("you can't go that direction");
+        next = NULL;
+    }
+
+    if (next)
+    {
+        next->proto.describe(next);
+    }
+
+    return next;
+}
+
+int Room_attack(void *self, int damage)
+{
+    Room *room = self;
+    Monster *monster = room->bad_guy;
+
+    if (monster)
+    {
+        monster->proto.attack(monster, damage);
+        // shouldn't this return the value from monster->_(attack) instead???
+        return 1;
+    }
+    else
+    {
+        printf("You flail in the air at nothing. (idiot...)\n");
+        return 0;
+    }
+}
+
+Object RoomProto = {
+    .move = Room_move,
+    .attack = Room_attack
+};
+
+
+/* Map type implementation */
+
+void *Map_move(void *self, Direction direction)
+{
+    Map *map = self;
+    Room *location = map->location;
+    Room *next = NULL;
+
+    next = location->proto.move(location, direction);
+
+    if (next)
+    {
+        map->location = next;
+    }
+
+    return next;
+}
+
+int Map_attack(void *self, int damage)
+{
+    Map *map = self;
+    Room *location = map->location;
+
+    return location->_(attack)(location, damage);
+}
+
+int Map_init(void *self)
+{
+    Map *map = self;
+
+    // make some rooms
+    Room *hall = NEW(Room, "The great Hall");
+    Room *throne = NEW(Room, "The throne room");
+    Room *arena = NEW(Room, "The arena, with the minotaur");
+    Room *kitchen = NEW(Room, "Kitchen, you have the knife now");
+
+    // put a monster in the arena
+    arena->bad_guy = NEW(Monster, "The evil minotaur");
+
+    // setup the room adjacencies
+    hall->north = throne;
+
+    throne->west = arena;
+    throne->east = kitchen;
+    throne->south = hall;
+
+    arena->east = throne;
+
+    kitchen->west = throne;
+
+    // start in the hall.  Though I'm not sure we ever actually use the start
+    // attr for anything, just the location.
+    map->start = hall;
+    map->location = hall;
+
+    return 1;
+}
+
+Object MapProto = {
+    .init = Map_init,
+    .move = Map_move,
+    .attack = Map_attack
+};
+
+
+/* core game logic */
+int process_input(Map *game)
+{
+    puts(">");
+
+    char in = getchar();
+    getchar(); // eats newline
+
+    // cheap, fragile solution to randomness ceiling... but it's only a game
+    int damage = rand() % 4;
+
+    switch (in)
+    {
+        case -1:
+            printf("Bye!");
+            return 0;
+            break;
+        case 'n':
+            game->_(move)(game, NORTH);
+            break;
+        case 's':
+            game->_(move)(game, SOUTH);
+            break;
+        case 'e':
+            game->_(move)(game, EAST);
+            break;
+        case 'w':
+            game->_(move)(game, WEST);
+            break;
+        case 'a':
+            game->_(attack)(game, damage);
+            break;
+        case 'l':
+            puts("You can go:");
+            if (game->location->north) puts("NORTH");
+            if (game->location->south) puts("SOUTH");
+            if (game->location->east) puts("EAST");
+            if (game->location->west) puts("WEST");
+            break;
+        default:
+            printf("What?: %d\n", in);
+    }
+
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    // seed random generator w/ the current time
+    srand(time(NULL));
+
+    // create game map
+    Map *game = NEW(Map, "The Hall of the Minotaur");
+
+    printf("You enter the ");
+    game->location->_(describe)(game->location);
+
+    int go;
+    do { go = process_input(game); } while (go);
+
+    return 0;
+}
---
+++ ex19/ex19.h
@@ -0,0 +1,60 @@
+#ifndef _ex19_h
+#define _ex19_h
+
+#include "object.h"
+
+
+/* in addition to all the base Object attributes, the Monster type
+ * also has a hit point counter.
+ * Using typedef like this lets us refer to the struct type as just "Monster",
+ * instead of "struct Monster"
+ */
+typedef struct {
+    Object proto;
+    int hit_points;
+} Monster;
+
+// Monster overrides these prototype methods
+int Monster_attack(void *self, int damage);
+int Monster_init(void *self);
+
+
+/* has pointer to a monster and to adjacent rooms in each direction.
+ * Any of these could be null (no enemy in this room, and/or no
+ * adjacent room in a given direction)
+ */
+struct Room {
+    Object proto;
+
+    Monster *bad_guy;
+
+    struct Room *north;
+    struct Room *south;
+    struct Room *east;
+    struct Room *west;
+};
+
+/* Instead of declaring the struct inside the typedef, we can also declare a
+ * "regular" struct and also use typedef, so we can refer to the struct type as
+ * _either_ "struct Room" or "Room"
+ */
+typedef struct Room Room;
+
+// Room overrides these prototype methods
+void *Room_move(void *self, Directon direction);
+int Room_attack(void *self, int damage);
+int Room_init(void *self);
+
+/* Has pointers to the starting room and the current room */
+typedef struct {
+    Object proto;
+    Room *start;
+    Room *location;
+} Map;
+
+// Map overrides these prototype methods
+void *Map_move(void *self, Direction direction);
+int Map_attack(void *self, int damage);
+int Map_init(void *self);
+
+#endif
